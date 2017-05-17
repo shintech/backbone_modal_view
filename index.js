3eee68e12ef80f0bea56e0168d37b491f70e3379
Michael Prather <mprather1@gmail.com>
2017-05-17 20:44:58 +0000

    new

---
+++ index.js
@@ -0,0 +1,15 @@
+const BackboneModalView = Backbone.Marionette.View.extend({
+  tagName: 'div',
+  className: 'modal fade hidden',
+  id: 'backbone-modal',
+  initialize: function (options) {
+    console.log(options)
+    this.template = options.template
+    this.render()
+  },
+  onRender: function () {
+    this.$el.modal('show').removeClass('hidden')
+  }
+})
+
+export default BackboneModalView
